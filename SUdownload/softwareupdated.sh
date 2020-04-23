ae8daf316bd7eb6881487af583a1f85a22e049fb
Minh Ton <ford.tonthat@gmail.com>
2020-04-23 10:09:07 +0700

    Update softwareupdated.sh

--- SUdownload/softwareupdated.sh
+++ SUdownload/softwareupdated.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "https://github.com/Minh-Ton/openinstallcreator/releases/download/beta4/release.zip"
+echo "https://github.com/Minh-Ton/openinstallcreator/releases/download/beta5.1/release.zip"
